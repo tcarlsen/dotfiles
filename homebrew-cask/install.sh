dd27121d32e761b59df6512028e00839c0b76f46
Thomas Carlsen <thomascarlsen86@gmail.com>
2014-04-07 11:37:18 +0200

    adding some brew cask apps

--- homebrew-cask/install.sh
+++ homebrew-cask/install.sh
@@ -8,3 +8,9 @@ brew install brew-cask
 
 # Install some quicklook-plugins
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json quicklook-csv
+
+# Install some browsers
+brew cask install google-chrome firefox
+
+# Install must have apps
+brew cask install alfred github gas-mask iterm2 dropbox licecap skype sequel-pro
