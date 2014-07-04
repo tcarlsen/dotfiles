cba42a5d574accc2895ce21835b236ef654f6f76
tcarlsen <thomascarlsen86@gmail.com>
2014-07-04 10:17:28 +0200

    now using arrow keys
    i’m no vim person so I like the arrow keys to navigate my windows around

--- hydra.symlink/init.lua
+++ hydra.symlink/init.lua
@@ -25,20 +25,20 @@ hotkey.bind(mash, "'", function() fnutils.map(window.visiblewindows(), ext.grid.
 hotkey.bind(mash, '+', function() ext.grid.adjustwidth( 1) end)
 hotkey.bind(mash, '-', function() ext.grid.adjustwidth(-1) end)
 
-hotkey.bind(mashshift, 'H', function() window.focusedwindow():focuswindow_west() end)
-hotkey.bind(mashshift, 'L', function() window.focusedwindow():focuswindow_east() end)
-hotkey.bind(mashshift, 'K', function() window.focusedwindow():focuswindow_north() end)
-hotkey.bind(mashshift, 'J', function() window.focusedwindow():focuswindow_south() end)
+hotkey.bind(mashshift, 'LEFT', function() window.focusedwindow():focuswindow_west() end)
+hotkey.bind(mashshift, 'RIGHT', function() window.focusedwindow():focuswindow_east() end)
+hotkey.bind(mashshift, 'UP', function() window.focusedwindow():focuswindow_north() end)
+hotkey.bind(mashshift, 'DOWN', function() window.focusedwindow():focuswindow_south() end)
 
 hotkey.bind(mash, 'M', ext.grid.maximize_window)
 
 hotkey.bind(mash, 'N', ext.grid.pushwindow_nextscreen)
 hotkey.bind(mash, 'P', ext.grid.pushwindow_prevscreen)
 
-hotkey.bind(mash, 'J', ext.grid.pushwindow_down)
-hotkey.bind(mash, 'K', ext.grid.pushwindow_up)
-hotkey.bind(mash, 'H', ext.grid.pushwindow_left)
-hotkey.bind(mash, 'L', ext.grid.pushwindow_right)
+hotkey.bind(mash, 'DOWN', ext.grid.pushwindow_down)
+hotkey.bind(mash, 'UP', ext.grid.pushwindow_up)
+hotkey.bind(mash, 'LEFT', ext.grid.pushwindow_left)
+hotkey.bind(mash, 'RIGHT', ext.grid.pushwindow_right)
 
 hotkey.bind(mash, 'U', ext.grid.resizewindow_taller)
 hotkey.bind(mash, 'O', ext.grid.resizewindow_wider)
