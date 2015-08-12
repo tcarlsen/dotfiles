b27f0c627974e52425b57c0bcc9c26b38c146d75
tcarlsen <thomascarlsen86@gmail.com>
2015-08-12 11:40:48 +0200

    adding some alias for npm, brew and cask

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -8,3 +8,18 @@ then
   alias ll="gls -l --color"
   alias la='gls -A --color'
 fi
+
+alias brewi="brew install"
+alias brewu="brew uninstall"
+alias brewup="brew upgrade"
+
+alias caski="brew cask install"
+alias casku="brew cask zap"
+
+alias npmi="npm install"
+alias npmu="npm uninstall"
+alias npmig="npm install -g"
+alias npmug="npm uninstall -g"
+
+alias php-server="php -S localhost:8000"
+alias lumen-server="php artisan serve"
