e3c28bb1a041c5d4f13e7410cdd2d69a27ff4f24
Thomas Carlsen <thomascarlsen86@gmail.com>
2026-05-19 13:31:07 +0200

    Modularize shell init for chruby, nvm, sdkman

---
+++ chruby/init.zsh
@@ -0,0 +1,4 @@
+# Ruby - chruby
+# https://github.com/postmodern/chruby
+source /opt/homebrew/opt/chruby/share/chruby/chruby.sh
+source /opt/homebrew/opt/chruby/share/chruby/auto.sh
---
+++ sdkman/init.zsh
@@ -0,0 +1,4 @@
+# Java - SDKMAN
+# https://sdkman.io/
+export SDKMAN_DIR=$(brew --prefix sdkman-cli)/libexec
+[[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]] && source "${SDKMAN_DIR}/bin/sdkman-init.sh"
